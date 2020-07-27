fbd5adc4408164a26ce9bc1b61119d5eacc73f67
Du4ert <du4ertb@gmail.com>
2020-07-27 22:26:14 +0300

    Lesson 1 homework

---
+++ js/main.js
@@ -0,0 +1,22 @@
+import { radioPlayerInit } from './radioPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+const deactivatePlayers = () => {
+    temp.style.display = 'none';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+};
+
+playerBtn.forEach( (btn, i) => btn.addEventListener('click', (item) => {
+        deactivatePlayers();
+        playerBlock[i].classList.add('active');
+}));
+
+videoPlayerInit();
+musicPlayerInit();
+radioPlayerInit();
---
+++ js/musicPlayer.js
@@ -0,0 +1,3 @@
+export const musicPlayerInit = () => {
+    console.log("musicPlayerInit -> Hello from music");
+} 
